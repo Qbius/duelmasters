ecf096ae00009f1ead3dd1d1ba44e139498f5620
Qbius <mich@lsurowka.email>
2020-03-26 13:28:21 +0100

    changes to deploy.sh so that cookies are different for qa and live

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 cd $1
 bin/dmdbserver stop
 tar -zxf tar/dmdbserver-0.1.0.tar.gz
+sed -i -e "s/cookiename/$1/g" releases/0.1.0/vm.args
 sed -i -e "s/portnumber/$2/g" releases/0.1.0/sys.config
 bin/dmdbserver start
