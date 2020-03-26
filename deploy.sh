9e3e255567d0965b60026f811c30f6456a5199f3
Qbius <mich@lsurowka.email>
2020-03-26 01:19:54 +0100

    deploy script fixes

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 cd $1
 bin/dmdbserver stop
 tar -zxf tar/dmdbserver-0.1.0.tar.gz
-sed -i -e "s/PORTNUMBER/$2/g" releases/0.1.0/sys.config
+sed -i -e "s/portnumber/$2/g" releases/0.1.0/sys.config
 bin/dmdbserver start
